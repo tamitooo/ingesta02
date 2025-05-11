5658a29cf93a7f2145348f61dd88a162dd14f146
tamitooo <162748368+tamitooo@users.noreply.github.com>
2025-05-10 22:52:28 -0500

    ..

--- ingesta.py
+++ ingesta.py
@@ -14,7 +14,7 @@ df = pd.read_sql(consulta, conexion)
 df.to_csv("data.csv", index=False)
 
 s3 = boto3.client('s3')
-bucket = "gcr-output-01"
+bucket = "tfg-output-02"
 s3.upload_file("data.csv", bucket, "data.csv")
 
 print("✅ Ingesta completada con éxito.")
