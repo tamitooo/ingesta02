6e069143413b0d23dad1f0c52780aabd80aa85e7
tamitooo <162748368+tamitooo@users.noreply.github.com>
2025-05-10 22:48:53 -0500

    .

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3-slim
+
 WORKDIR /programas/ingesta
-RUN pip3 install boto3
+
+RUN pip3 install boto3 pandas mysql-connector-python
+
 COPY . .
+
 CMD [ "python3", "./ingesta.py" ]
--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,20 @@
+import mysql.connector
+import pandas as pd
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+conexion = mysql.connector.connect(
+    host="mysql_c", 
+    user="root",
+    password="utec",
+    database="basededatos"
+)
+
+consulta = "SELECT * FROM productos;"
+df = pd.read_sql(consulta, conexion)
+df.to_csv("data.csv", index=False)
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+bucket = "gcr-output-01"
+s3.upload_file("data.csv", bucket, "data.csv")
 
-print("Ingesta completada")
+print("✅ Ingesta completada con éxito.")
